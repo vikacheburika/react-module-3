cc5a636c3adae4f8b70f092ddfdb02d9f1d15f7b
vikacheburika <vikacheburika@gmail.com>
2026-03-02 19:53:44 +0200

    practiced CRUD and useState , created a to-do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import OneProduct from './components/OneProduct/Oneproduct.jsx'
-import Productlist from './components/ProductList/ProductList.jsx'
 import './App.css'
-import ProductList from './components/ProductList/ProductList.jsx'
-import { v4 as uuidv4 } from "uuid"
+import TaskList from './components/TaskList/TaskList'
+import { useState } from "react"
 
 
 function App() {
@@ -10,8 +8,8 @@ function App() {
 
   return (
     <>
-
-      <ProductList/>
+      <TaskList/>
+      
     </>
   )
 }
